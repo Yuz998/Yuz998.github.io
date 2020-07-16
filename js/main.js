243b69551877f28ea62736eb12bedcffe82d0b0a
Yuz998 <yuzhao963@gmail.com>
2020-07-16 19:14:22 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -789,3 +789,9 @@ $(function () {
     }
   })
 })
+
+
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
